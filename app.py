d2ba6aba54d0a0da8228c3fa664623d3542f8c81
smith.parekh <smith.parekh@paritycube.com>
2026-03-04 18:12:19 +0530

    Day 3: Added PDF parser and resume upload feature

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import streamlit as st
+from parser import extract_text_from_pdf
 
 st.title("AI Resume Analyzer")
 
-st.write("Day 1 Setup Successful")
+uploaded_file = st.file_uploader("Upload your Resume (PDF)", type=["pdf"])
 
+if uploaded_file is not None:
+    
+    resume_text = extract_text_from_pdf(uploaded_file)
+
+    st.subheader("Extracted Resume Text")
+    st.write(resume_text[:2000])
